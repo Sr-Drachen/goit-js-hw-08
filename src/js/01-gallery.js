406fc5c614a93476dbd81e38a3812a850e38fe14
Sr-Drachen <andresss10011@gmail.com>
2023-05-24 17:53:34 -0500

    mmm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from '/src/js/gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
