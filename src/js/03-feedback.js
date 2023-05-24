da7ffa5f128c8322f19911295eff7b7b0beeea4e
Sr-Drachen <andresss10011@gmail.com>
2023-05-24 17:06:13 -0500

    Si

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,5 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
+import '/css/common.css';
+import '/css/03-feedback.css';
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
