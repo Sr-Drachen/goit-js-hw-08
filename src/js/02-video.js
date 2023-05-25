f9bd7414900e72e5a549323cda76123ea98cb564
Sr-Drachen <andresss10011@gmail.com>
2023-05-25 16:03:44 -0500

    MMmm

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,34 @@
-import '/src/css/common.css';
+import '../css/common.css';
 import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
 const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 const onPlay = function (data) {
-  const stringifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, stringifyData);
+  const strigifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, strigifyData);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function resumePlayback() {
-  if (localStorage.getItem(TIME_KEY) === null) {
+  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
     return;
   }
   const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
   if (paused) {
-    player.setCurrentTime(paused).catch(function (error) {
-      switch (error.name) {
-        case 'Error':
-          break;
-        default:
-          break;
-      }
-    });
+    player
+      .setCurrentTime(paused)
+      .then(function (seconds) {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'Error':
+            break;
+          default:
+            break;
+        }
+      });
   }
 }
-player.on('ready', resumePlayback);
+resumePlayback();
