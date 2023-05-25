d0a11345f18b54f7d0f8d3e09944756e493d5280
Sr-Drachen <andresss10011@gmail.com>
2023-05-25 15:50:25 -0500

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,28 +6,25 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 const onPlay = function (data) {
-  const strigifyData = JSON.stringify(data);
-  localStorage.setItem(TIME_KEY, strigifyData);
+  const stringifyData = JSON.stringify(data);
+  localStorage.setItem(TIME_KEY, stringifyData);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function resumePlayback() {
-  if (JSON.parse(localStorage.getItem(TIME_KEY)) === null) {
+  if (localStorage.getItem(TIME_KEY) === null) {
     return;
   }
   const paused = JSON.parse(localStorage.getItem(TIME_KEY))['seconds'];
   if (paused) {
-    player
-      .setCurrentTime(paused)
-      .then(function (seconds) {})
-      .catch(function (error) {
-        switch (error.name) {
-          case 'Error':
-            break;
-          default:
-            break;
-        }
-      });
+    player.setCurrentTime(paused).catch(function (error) {
+      switch (error.name) {
+        case 'Error':
+          break;
+        default:
+          break;
+      }
+    });
   }
 }
-resumePlayback();
+player.on('ready', resumePlayback);
